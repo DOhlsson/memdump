9ebda68466707f1ffe51ff2988801822680767fc
Daniel Ohlsson <dohlsson89@gmail.com>
2019-10-23 21:02:29 +0200

    memshove now supports offset and longer data

--- src/memshove.c
+++ src/memshove.c
@@ -5,39 +5,52 @@
 
 int main(int argc, char** argv)
 {
-  if (argc != 5) {
-    printf("Usage: %s <pid> <addr> <len> <byte>\n", argv[0]);
+  if (argc != 6) {
+    printf("Usage: %s <pid> <addr> <offset> <len> <data>\n", argv[0]);
     return 1;
   }
 
   pid_t pid = 0;
   void* addr = NULL;
+  unsigned int offset = 0;
   size_t len = 0;
-  unsigned char byte = 0;
+  unsigned char data[256];
 
   sscanf(argv[1], "%d", &pid);
   sscanf(argv[2], "%p", &addr);
-  sscanf(argv[3], "%lu", &len);
-  sscanf(argv[4], "%02hhx", &byte);
+  sscanf(argv[3], "%x", &offset);
+  sscanf(argv[4], "%lu", &len);
 
-  printf("%d %p %lu %x\n", pid, addr, len, byte);
+  /* parse data from hex */
+
+  if (len > 256 || len < 0) {
+    printf("Error: len argument out of bounds\n");
+    exit(1);
+  }
+
+  for (int i = 0; i < len; i++) {
+    sscanf(argv[5] + (i * 2), "%02hhx", data + i);
+    printf("data: %x\n", data[i]);
+  }
+
+  printf("Writing %lu bytes of data to pid %d at %p+%x\n", len, pid, addr, offset);
 
   /* Write mem */
 
   struct iovec local;
   struct iovec remote;
 
-  char* buf = malloc(len * sizeof(char));
-  buf[0] = byte;
-
-  local.iov_base = buf;
+  local.iov_base = data;
   local.iov_len = len;
 
-  remote.iov_base = addr;
+  remote.iov_base = (char*)addr + offset;
   remote.iov_len = len;
 
   ssize_t bytesw = process_vm_writev(pid, &local, 1, &remote, 1, 0);
-  perror("vm write");
+  if (bytesw == -1) {
+    perror("Error writing data");
+    exit(1);
+  }
 
   printf("Wrote %lu bytes\n", bytesw);
 
